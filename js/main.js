8acb98ba6c692e93cfbd534163b2fe1332642d01
Laila Seif <lailaseif710@gmail.com>
2025-02-04 13:03:55 -0500

    added ids to svgs & using ex file for javascript
    added ids to my svgs
    now, i am using the example file provided in FOL class to again figure out logging icons, giving them functions, etc. I noticed that all the
    
    const iconNameHere = document.querySelector OR querySelectorAll
    
    were at the top...
    
    function Peremeter () {
    console.log(this.id);
    
    was in the middle...and eventListeners  and .forEach, etc were at the bottom. Though this order for me is a bit confusing, I will try to get it right since I do now have a better understanding what everything does.
    I am trying to do it the same order as the example file and see what works (play around with it)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 console.log("Connected JS for Dr.Nut SVG page")
 
+
+const allIcons = document.querySelectorAll('svg')
+const almondIcon = document.querySelector('#almond-icon')
+const hazelIcon = document.querySelector('#hazel-icon')
+const pistaIcon = document.querySelector('#pista-icon')
+const macaIcon = document.querySelector('#maca-icon')
+const almondVIcon = document.querySelector('#almond-v-icon')
+const cocoVIcon = document.querySelector('#coco-v-icon')
+
+function LogIt () {
+    console.log(this.id);
+};
+
+
+almondIcon.addEventListener('click', almondIcon
+
+)
